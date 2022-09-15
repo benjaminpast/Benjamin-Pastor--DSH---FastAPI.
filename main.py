253d75073a02ea0a44f1c83cf61ac84ae4e23a9d
benjaminpast <104089435+benjaminpast@users.noreply.github.com>
2022-09-15 20:40:16 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,27 +14,27 @@ async def database_connect():
 async def database_disconnect():
     await database.disconnect()
 
-
-@app.get("/Año con más carreras")
+#Año con más carreras
+@app.get("/Año_más_carreras")
 async def fetch_data(id: int):
     consulta = "SELECT year, count(raceId) AS carreras FROM races GROUP BY year ORDER BY carreras DESC LIMIT {}".format(str(id))
     results = await database.fetch_all(query=consulta)
     return  results
 
 #Pilotocon mayor cantidad de primeros puestos
-@app.get("/Piloto")
+@app.get("/Piloto_con_mas_1")
 async def data():
     consulta = "SELECT r.driverId ,surname,forename, count(r.position) FROM results r JOIN drivers d ON r.driverId = d.driverId WHERE Position = 1".format(str(id))
     results = await database.fetch_all(query=consulta)
     return  results
-
-@app.get("/Nombre del circuito más corrido")
+#Nombre del circuito_más_corrido
+@app.get("/circuito_más_corrido")
 async def fetch_data(id: int):
     consulta = "SELECT c.name, count(raceId) as carreras   FROM races  r join circuits c on c.circuitId = r.circuitId GROUP BY c.name ORDER BY carreras DESC LIMIT 1".format(str(id))
     results = await database.fetch_all(query=consulta)
     return  results
-
-@app.get("/Piloto con mayor cantidad de puntos en total, cuyo constructor sea de nacionalidad sea American o British")
+#Piloto con mayor cantidad de puntos en total, cuyo constructor sea de nacionalidad sea American o British
+@app.get("/Piloto_mas_puntos")
 async def fetch_data(id: int):
     consulta = "SELECT d.forename, d.surname ,r.driverId, sum(r.points) as puntaje_total FROM results r JOIN drivers d on r.driverId = d.driverId JOIN constructors c on r.constructorId = c.constructorId WHERE c.nationality = 'British' or c.nationality = 'American' GROUP BY r.driverId ORDER BY puntaje_total DESC LIMIT {}".format(str(id))
     results = await database.fetch_all(query=consulta)
